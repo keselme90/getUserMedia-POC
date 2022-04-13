2d4489245ae30cbc5aeae35f7d5772dc9be437e4
keselme <keselme90@gmail.com>
2022-04-13 21:01:04 +0300

    Initial Commit

---
+++ URL_POC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URL_POC
+//
+//  Created by Edward Keselman on 13/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
